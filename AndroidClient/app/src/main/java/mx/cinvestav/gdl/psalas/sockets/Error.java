819b945e068c861bbb236013c34c6d5c2ed8ec48
Pedro Ivan Salas <psalas@localhost.localdomain>
2015-03-06 23:36:28 -0600

    Android Client

---
+++ AndroidClient/app/src/main/java/mx/cinvestav/gdl/psalas/sockets/Error.java
@@ -0,0 +1,16 @@
+package mx.cinvestav.gdl.psalas.sockets;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Error extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+    }
+}
